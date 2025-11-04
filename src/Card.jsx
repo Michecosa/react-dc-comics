b9b773194f515996573101e66bc7b83a208a90bc
Michela <micheladellagatta1@gmail.com>
2025-11-04 15:54:37 +0100

    Create Card structure

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import "./Card.css";
+
+function Card({ imageUrl, text }) {
+  return (
+    <div className="card-container">
+      <div>
+        <img src={imageUrl} alt="card" />
+      </div>
+      <div>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
