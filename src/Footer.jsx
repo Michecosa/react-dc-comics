4587b6d10ab4113c798bba7fa77e85a1581cf5c4
Michela <micheladellagatta1@gmail.com>
2025-11-04 16:46:21 +0100

    Separate footer in two main areas

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,60 +7,63 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer-container">
-      <div>
-        <a href="#" className="btn btn-signup">
-          SIGN-UP NOW!
-        </a>
+    <>
+      <div id="top-footer"></div>
+      <div id="bottom-footer" className="footer-container">
+        <div>
+          <a href="#" className="btn btn-signup">
+            SIGN-UP NOW!
+          </a>
+        </div>
+        <div id="follow-us">
+          <h2>Follow Us</h2>
+          <div className="footer-logo-container">
+            <a href="#">
+              <img
+                src={footerFacebook}
+                className="footer-logo"
+                alt="footer social logo"
+              />
+            </a>
+          </div>
+          <div className="footer-logo-container">
+            <a href="#">
+              <img
+                src={footerTwitter}
+                className="footer-logo"
+                alt="footer social logo"
+              />
+            </a>
+          </div>
+          <div className="footer-logo-container">
+            <a href="#">
+              <img
+                src={footerYoutube}
+                className="footer-logo"
+                alt="footer social logo"
+              />
+            </a>
+          </div>
+          <div className="footer-logo-container">
+            <a href="#">
+              <img
+                src={footerPinterest}
+                className="footer-logo"
+                alt="footer social logo"
+              />
+            </a>
+          </div>
+          <div className="footer-logo-container">
+            <a href="#">
+              <img
+                src={footerPeriscope}
+                className="footer-logo"
+                alt="footer social logo"
+              />
+            </a>
+          </div>
+        </div>
       </div>
-      <div id="follow-us">
-        <h2>Follow Us</h2>
-        <div className="footer-logo-container">
-          <a href="#">
-            <img
-              src={footerFacebook}
-              className="footer-logo"
-              alt="footer social logo"
-            />
-          </a>
-        </div>
-        <div className="footer-logo-container">
-          <a href="#">
-            <img
-              src={footerTwitter}
-              className="footer-logo"
-              alt="footer social logo"
-            />
-          </a>
-        </div>
-        <div className="footer-logo-container">
-          <a href="#">
-            <img
-              src={footerYoutube}
-              className="footer-logo"
-              alt="footer social logo"
-            />
-          </a>
-        </div>
-        <div className="footer-logo-container">
-          <a href="#">
-            <img
-              src={footerPinterest}
-              className="footer-logo"
-              alt="footer social logo"
-            />
-          </a>
-        </div>
-        <div className="footer-logo-container">
-          <a href="#">
-            <img
-              src={footerPeriscope}
-              className="footer-logo"
-              alt="footer social logo"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
