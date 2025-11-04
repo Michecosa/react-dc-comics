bce02cc91bef0858cd671029ce5cc81a42c13b9e
Michela <micheladellagatta1@gmail.com>
2025-11-04 14:49:10 +0100

    Customize navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,45 @@
 import Logo from "./Logo";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav>
-      <div>
+      <div className="logo-container">
         <Logo />
       </div>
 
-      <div>
+      <div className="links-container">
         <ul>
-          <li>Characters</li>
-          <li>Comics</li>
-          <li>Movies</li>
-          <li>Tv</li>
-          <li>Games</li>
-          <li>Collectibles</li>
-          <li>Videos</li>
-          <li>Fans</li>
-          <li>News</li>
-          <li>Shop</li>
+          <li>
+            <a href="#">Characters</a>
+          </li>
+          <li>
+            <a href="#">Comics</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">Tv</a>
+          </li>
+          <li>
+            <a href="#">Games</a>
+          </li>
+          <li>
+            <a href="#">Collectibles</a>
+          </li>
+          <li>
+            <a href="#">Videos</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
         </ul>
       </div>
     </nav>
