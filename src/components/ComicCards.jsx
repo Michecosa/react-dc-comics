11f5f15a8c42c2355f720347730eb19719399ceb
Michela <micheladellagatta1@gmail.com>
2025-11-05 12:17:59 +0100

    Import comics from js

--- src/components/ComicCards.jsx
+++ src/components/ComicCards.jsx
@@ -1,1 +1,15 @@
 import "./ComicCards.css";
+import comics from "../data/comics.js";
+
+export default function ComicCards() {
+  return (
+    <div className="comic-cards">
+      {comics.map((comic, index) => (
+        <div key={index} className="comic-card">
+          <img src={comic.thumb} alt={comic.title} />
+          <h3>{comic.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
