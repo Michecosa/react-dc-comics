28a89dbbba4599f2031c83a0b4fb359a556fe5e1
Michela <micheladellagatta1@gmail.com>
2025-11-05 12:19:23 +0100

    Display all comic infos

--- src/components/ComicCards.jsx
+++ src/components/ComicCards.jsx
@@ -4,10 +4,12 @@ import comics from "../data/comics.js";
 export default function ComicCards() {
   return (
     <div className="comic-cards">
-      {comics.map((comic, index) => (
-        <div key={index} className="comic-card">
+      {comics.map((comic) => (
+        <div key={comic.id} className="comic-card">
           <img src={comic.thumb} alt={comic.title} />
           <h3>{comic.title}</h3>
+          <p>{comic.series}</p>
+          <span>{comic.price}</span>
         </div>
       ))}
     </div>
