e157581bc8776b5b7ba66bb5f5e6490e7763d9ef
Michela <micheladellagatta1@gmail.com>
2025-11-05 14:53:06 +0100

    Change logo size based on the window width

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,5 +2,5 @@ import "./Logo.css";
 import dcLogo from "../assets/img/dc-logo.png";
 
 export default function Logo() {
-  return <img src={dcLogo} className="logo logo-spin" alt="DC logo" />;
+  return <img src={dcLogo} className="logo col-lg-12" alt="DC logo" />;
 }
