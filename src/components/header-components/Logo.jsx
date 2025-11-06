ab3a4fb5ddfe5ce24c9073bf1aeb3ede1142da72
Michela <micheladellagatta1@gmail.com>
2025-11-06 13:10:06 +0100

    Create dedicated folder for header elements

--- src/components/header-components/Logo.jsx
+++ src/components/header-components/Logo.jsx
@@ -1,5 +1,5 @@
 import "./Logo.css";
-import dcLogo from "../assets/img/dc-logo.png";
+import dcLogo from "../../assets/img/dc-logo.png";
 
 export default function Logo() {
   return <img src={dcLogo} className="logo col-lg-12" alt="DC logo" />;
