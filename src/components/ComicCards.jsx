301de9264c3e0ed9cc6525374f5e18bb8c36bffb
Michela <micheladellagatta1@gmail.com>
2025-11-06 11:52:33 +0100

    Rewrite ComicCards logic and add condition when comic has id 5 to replace empty thumb

--- src/components/ComicCards.jsx
+++ src/components/ComicCards.jsx
@@ -1,23 +1,19 @@
 import "./ComicCards.css";
 import comics from "../data/comics.js";
+import ComicCard from "./ComicCard";
 
 export default function ComicCards() {
   return (
     <div id="comic-cards" className="row">
       {comics.map((comic) => (
-        <div
+        <ComicCard
           key={comic.id}
-          className="comic-card col-xl-2 col-lg-3 col-md-6 col-sm-12"
-        >
-          <div className="image-wrapper">
-            <img
-              src={comic.id === 5 ? comics[5].thumb : comic.thumb}
-              alt={comic.title}
-              className={comic.id === 1 || comic.id === 12 ? "zoom" : ""}
-            />
-          </div>
-          <p>{comic.series}</p>
-        </div>
+          comic={
+            comic.id === 5
+              ? { ...comic, thumb: comics[5] && comics[5].thumb }
+              : comic
+          }
+        />
       ))}
     </div>
   );
